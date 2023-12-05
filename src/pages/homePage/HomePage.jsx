86ff90dd51318ba1b369c791971995d86101d572
Daniel Johansen <VMGX68@motorolasolutions.com>
2023-12-05 22:01:13 +0100

    Update Routing, Add template pages

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import { Card, Modal, Container, Button, Row, Col } from "react-bootstrap";
 
-import './homePage.styles.css';
+import "./homePage.styles.css";
 
 const HomePage = () => {
-return (
-    <div>This is the HomePage</div>
-  )
-}
+  return (
+    <div>
+      <Container fluid>
+        <Card>
+          <Card.Header>
+            <Card.Title>Title</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <h1>Something</h1>
+            <p>Will figure out later</p>
+          </Card.Body>
+          <Card.Footer>
+            <div className="d-grid gap-2">
+              <Button variant="secondary" size="lg">
+                --
+              </Button>
+              <Button variant="primary" size="lg">
+                --
+              </Button>
+            </div>
+          </Card.Footer>
+        </Card>
+      </Container>
+    </div>
+  );
+};
 
 export default HomePage;
